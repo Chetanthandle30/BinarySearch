4f52e44b81397abf09cd5dc7e542965ab8fc151c
Chetanthandle30 <87128154+Chetanthandle30@users.noreply.github.com>
2022-10-03 15:31:00 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def BinarySearch(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    while low <= high:
+        if low <= high:
+            mid = (low + high) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+
+arr = [1, 2, 3, 4, 5]
+print(BinarySearch(arr, 4))
